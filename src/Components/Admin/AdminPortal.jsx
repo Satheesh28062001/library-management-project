5d869cab620e2ad6e3196008eb4477332918cfee
Satheesh28062001 <satheeshwaran95@gmail.com>
2024-07-25 18:45:15 +0530

    Update AdminPortal.jsx

--- src/Components/Admin/AdminPortal.jsx
+++ src/Components/Admin/AdminPortal.jsx
@@ -5,7 +5,6 @@ import Home from '../Home'
 import Books from '../Books'
 import ReadBooks from '../ReadBooks'
 import AddBooks from '../AddBooks'
-import About from '../About'
 import Users from '../Users'
 import AddUsers from './AddUsers'
 import Logout from '../Logout'
@@ -19,7 +18,6 @@ const AdminPortal = () => {
         <Route element={<Books/>} path='/books'/>
         <Route element={<ReadBooks/>} path='/readbooks/:id'/>
         <Route element={<AddBooks/>} path='/addbooks'/>
-        {/* <Route element={<About/>} path='/about'/> */}
         <Route element={<Users/>} path='/users'/>
         <Route element={<AddUsers/>} path='addusers'/>
         <Route element={<Logout/>} path='logout'/>
